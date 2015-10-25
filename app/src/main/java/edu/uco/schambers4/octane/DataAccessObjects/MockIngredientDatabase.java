be935021e6aac33797ab16c14cf87efeec2f6ee1
Steven <stevenchambers.exe@gmail.com>
2015-10-25 14:35:43 -0500

    create mock database for ingredient display design

---
+++ app/src/main/java/edu/uco/schambers4/octane/DataAccessObjects/MockIngredientDatabase.java
@@ -0,0 +1,55 @@
+package edu.uco.schambers4.octane.DataAccessObjects;
+
+import com.annimon.stream.Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uco.schambers4.octane.Models.IIngredient;
+import edu.uco.schambers4.octane.Models.Ingredient;
+
+/**
+ * Created by Steven Chambers on 10/25/2015.
+ */
+public class MockIngredientDatabase implements IIngredientDatabase
+{
+    List<IIngredient> ingredients;
+
+    public MockIngredientDatabase()
+    {
+        ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient("Steak", 8, "ounces"));
+        ingredients.add(new Ingredient("Cheese", 2 , "cups"));
+        ingredients.add(new Ingredient("Bacon", 1 , "rasher"));
+    }
+
+    @Override
+    public void addIngredientToCollection(IIngredient ingredient)
+    {
+
+    }
+
+    @Override
+    public void removeIngredientFromCollection(IIngredient ingredient)
+    {
+
+    }
+
+    @Override
+    public List<IIngredient> getCollectionAsList()
+    {
+        return ingredients;
+    }
+
+    @Override
+    public Stream<IIngredient> getCollectionAsStream()
+    {
+        return Stream.of(ingredients);
+    }
+
+    @Override
+    public void saveChanges()
+    {
+
+    }
+}
