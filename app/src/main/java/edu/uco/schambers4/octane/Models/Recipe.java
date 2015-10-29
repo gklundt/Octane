1232565f0858d4d01abc9d0bdc91ffdf28abd9bc
Steven <stevenchambers.exe@gmail.com>
2015-10-29 13:46:17 -0500

    add initial data structure for recipes

---
+++ app/src/main/java/edu/uco/schambers4/octane/Models/Recipe.java
@@ -0,0 +1,53 @@
+package edu.uco.schambers4.octane.Models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Steven Chambers on 10/29/2015.
+ */
+public class Recipe implements IIngredient
+{
+    private String name;
+    private Map<IIngredient, Double> ingredientAndAmountMap;
+
+    public Recipe(String name)
+    {
+        this.name = name;
+        ingredientAndAmountMap = new HashMap<>();
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public int getCalories()
+    {
+        int calorieSum = 0;
+        for (Map.Entry<IIngredient, Double> cursor : ingredientAndAmountMap.entrySet())
+        {
+            IIngredient ingredient = cursor.getKey();
+            calorieSum += ingredientAndAmountMap.get(ingredient) * ingredient.getCalories();
+        }
+        return calorieSum;
+    }
+
+    public Map<IIngredient, Double> getIngredientQuantityMap()
+    {
+        return ingredientAndAmountMap;
+    }
+
+    public void addIngredient(IIngredient ingredient, double quantity)
+    {
+        ingredientAndAmountMap.put(ingredient, quantity);
+    }
+    public void removeIngredient(IIngredient ingredient)
+    {
+        ingredientAndAmountMap.remove(ingredient);
+    }
+}
