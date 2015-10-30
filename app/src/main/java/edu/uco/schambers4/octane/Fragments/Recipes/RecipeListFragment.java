7bbfee5f6082a632ce0776666399c4ec25b3cd91
Steven <stevenchambers.exe@gmail.com>
2015-10-29 20:51:16 -0500

    implement recipe deletion

--- app/src/main/java/edu/uco/schambers4/octane/Fragments/Recipes/RecipeListFragment.java
+++ app/src/main/java/edu/uco/schambers4/octane/Fragments/Recipes/RecipeListFragment.java
@@ -1,7 +1,9 @@
 package edu.uco.schambers4.octane.Fragments.Recipes;
 
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
@@ -33,6 +35,8 @@ public class RecipeListFragment extends Fragment
     FloatingActionButton recipeAddFab;
 
     private RecipeRespository recipeRespository;
+
+    private ArrayAdapter<IIngredient> recipeAdapter;
 
     public RecipeListFragment()
     {
@@ -74,17 +78,51 @@ public class RecipeListFragment extends Fragment
     {
         ((MainActivity) getActivity()).launchFragment(fragment);
     }
+
     private void bindRecipesToListView()
     {
-        ArrayAdapter<IIngredient> adapter = new ArrayAdapter<IIngredient>(
+        recipeAdapter= new ArrayAdapter<IIngredient>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
                 recipeRespository.getCollectionAsList()
-                );
-        recipeListView.setAdapter(adapter);
+        );
+        recipeListView.setAdapter(recipeAdapter);
         recipeListView.setOnItemClickListener((parent, view, position, id) -> {
             launchFragmentToEditRecipe(position);
         });
+        recipeListView.setOnItemLongClickListener((parent, view, position, id) -> {
+            IIngredient recipe = recipeAdapter.getItem(position);
+            showDeleteRecipePopUp(recipe);
+            return true;
+        });
+    }
+
+    private void showDeleteRecipePopUp(IIngredient recipe)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle(String.format("Delete %s?", recipe.getName()))
+                .setPositiveButton(
+                        "Delete",
+                        (dialog, which) -> {
+                            recipeRespository.removeRecipeFromCollection(recipe);
+                            recipeRespository.saveChanges();
+                            refreshView();
+                        }
+                )
+                .setNegativeButton(
+                        "Cancel",
+                        (dialog, which) -> {
+                            dialog.dismiss();
+                        }
+                ).show();
+    }
+
+    private void refreshView()
+    {
+        recipeAdapter.clear();
+        recipeAdapter.addAll(recipeRespository.getCollectionAsList());
+        recipeAdapter.notifyDataSetChanged();
     }
 
     private void launchFragmentToEditRecipe(int position)
@@ -93,6 +131,13 @@ public class RecipeListFragment extends Fragment
         launchFragment(frag);
     }
 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        recipeRespository.refreshData();
+        refreshView();
+    }
 
     @Override
     public void onDestroyView()
