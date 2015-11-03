4bc23a3dfc1c2f1b27d0a624f2540c3163da9e9a
ParaSwarm <kodamn@gmail.com>
2015-11-03 10:31:27 -0600

    Changed to Telerik Calender.

--- app/src/main/java/edu/uco/schambers4/octane/Fragments/DashboardFragment.java
+++ app/src/main/java/edu/uco/schambers4/octane/Fragments/DashboardFragment.java
@@ -1,21 +1,28 @@
 package edu.uco.schambers4.octane.Fragments;
 
+import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
 import android.widget.FrameLayout;
 
+import com.telerik.widget.calendar.CalendarSelectionMode;
+import com.telerik.widget.calendar.RadCalendarView;
+import com.telerik.widget.calendar.events.Event;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import edu.uco.schambers4.octane.Activities.MainActivity;
+import edu.uco.schambers4.octane.DataAccessObjects.Workout.MockWorkoutRepository;
+import edu.uco.schambers4.octane.Models.Workout.Workout;
 import edu.uco.schambers4.octane.R;
 
 
@@ -24,7 +31,7 @@ public class DashboardFragment extends Fragment {
     @Bind(R.id.fragment_container_dashboard)
     FrameLayout fragmentContainer;
     @Bind(R.id.calendarView)
-    CalendarView calendar;
+    RadCalendarView calendar;
     @Bind(R.id.add_meal_fab)
     FloatingActionButton mealFab;
     @Bind(R.id.add_workout_fab)
@@ -41,18 +48,39 @@ public class DashboardFragment extends Fragment {
     }
 
     private void initializeCalender(){
-//        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.colorAccent));
-        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.colorPrimary));
-        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.colorPrimary));
-        calendar.setSelectedDateVerticalBar(R.color.colorPrimary);
+        calendar.setSelectionMode(CalendarSelectionMode.Single);
 
-        calendar.setOnDateChangeListener((view, year, month, day) -> {
-            Calendar cal = Calendar.getInstance();
-            cal.set(year, month, day);
-            Date date = cal.getTime();
-
+        calendar.setOnSelectedDatesChangedListener((context) -> {
+            Date date = new Date(context.newSelection().get(0));
             launchDetailsForDateFragment(date);
         });
+
+        MockWorkoutRepository mockWorkoutRepo = new MockWorkoutRepository();
+        List<Workout> workouts = mockWorkoutRepo.getAllWorkouts(getActivity().getApplicationContext());
+
+
+        List<Event> events = new ArrayList<>();
+        Calendar dateProcessor = Calendar.getInstance();
+
+        for(Workout workout : workouts) {
+            dateProcessor.set(Calendar.HOUR_OF_DAY, 20);
+            dateProcessor.set(Calendar.MINUTE, 0);
+            dateProcessor.set(Calendar.SECOND, 0);
+            dateProcessor.set(Calendar.MILLISECOND, 0);
+
+            long eventStart = dateProcessor.getTimeInMillis();
+
+            dateProcessor.add(Calendar.HOUR, 3);
+            long eventEnd = dateProcessor.getTimeInMillis();
+
+            Event event = new Event(workout.getName(), eventStart, eventEnd);
+
+            events.add(event);
+
+            dateProcessor.add(Calendar.DAY_OF_YEAR, 1);
+        }
+
+        calendar.getEventAdapter().setEvents(events);
     }
 
     @Override
