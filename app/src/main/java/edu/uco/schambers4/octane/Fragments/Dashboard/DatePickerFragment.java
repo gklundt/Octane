647a79a0c62b79e9d238e5d72de158347d8ac930
ParaSwarm <kodamn@gmail.com>
2015-11-17 10:13:28 -0600

    Can now schedule Workouts and Meals from the Dashboard.

---
+++ app/src/main/java/edu/uco/schambers4/octane/Fragments/Dashboard/DatePickerFragment.java
@@ -0,0 +1,35 @@
+package edu.uco.schambers4.octane.Fragments.Dashboard;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * Created by ParaSwarm on 9/20/2015.
+ */
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        target.setText(Integer.toString(month + 1) + "/" + Integer.toString(day) + "/" + Integer.toString(year) );
+    }
+
+    public TextView target;
+}
